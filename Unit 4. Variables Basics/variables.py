3ed39aebed598b724749025d959d66be64778c2a
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-02 17:22:10 +0200

    Unit 4 - Variables basics

---
+++ Unit 4. Variables Basics/variables.py
@@ -0,0 +1,17 @@
+score = 500
+print(score)
+score = score + 1
+print(score)
+
+validVariableName = True;
+valid_variable_name = True;
+
+score += 1
+score -= 1
+score *= 2
+score /= 2
+score %= 2
+score **= 2
+score //= 2
+
+print(score)
