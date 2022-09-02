2ab02c75adb1cdcc39d45587502cf0f9a14b09a4
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-02 14:28:28 +0200

    Unit 3 - numbers and operators

---
+++ Unit 3. Python Numbers/numbers.py
@@ -0,0 +1,12 @@
+print(type(12))
+print(type(2.9))
+print(1 + 2)
+print(5 - 2.5)
+print(3 * 4)
+print(10 / 2)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 3)
+print(2 ** 0.5)
+#comments are ignored by the interpreter
+
