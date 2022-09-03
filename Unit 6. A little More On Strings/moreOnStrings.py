41291ff73439811ffb053223e00dced63ea43d83
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-03 13:26:50 +0200

    Unit 6 - more on strings

---
+++ Unit 6. A little More On Strings/moreOnStrings.py
@@ -0,0 +1,15 @@
+index_string = "0123456789"
+print(index_string[1:3])
+print(index_string[1:])
+print(index_string[:3])
+print(index_string[1:8:2])
+print(index_string[::2])
+print(index_string[::-1])
+print(index_string[-1])
+print(index_string[-3:-1])
+print(index_string[-3:])
+print(index_string[:-3])
+escape_string = "This is a \"double quoted\" string inside a single quoted string."
+print(escape_string)
+escape_string = 'This is a "double quoted" string inside a single quoted string.'
+print(escape_string)
