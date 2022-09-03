997ab493082e1bacf7f7b5ac06308ad72f1889db
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-03 23:08:33 +0200

    Unit 8. The world of methods examples

---
+++ Unit 8. The world of methods/methods.py
@@ -0,0 +1,18 @@
+example_string = "This is a string"
+print(example_string.upper())
+print(example_string.lower())
+print(example_string.title())
+print(example_string.capitalize())
+print(example_string.swapcase())
+print(example_string.count("is"))
+print(example_string.find("is"))
+
+answer = "  cotton candy\n"
+print(answer.strip())
+str_replace = "I like to eat apples"
+print(str_replace.replace("apples", "oranges"))
+
+product = " Prune Juice  "
+print(product.strip().lower().replace(" ", "-"))
+
+
