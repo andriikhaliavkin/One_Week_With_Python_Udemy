12aacc7a7d9a5aa36085e2e4eb0a9429085322e2
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-03 12:33:40 +0200

    Unit 5 - Strings Basics

---
+++ Unit 5. Strings Basics/strings.py
@@ -0,0 +1,16 @@
+color = "Magenta"
+print(color)
+some_spaces = "  "
+print(some_spaces)
+age = "56"
+url = "https://www.google.com"
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+string_index = "0123456789"
+print(string_index[0])
+
+user = None
+print(user)
