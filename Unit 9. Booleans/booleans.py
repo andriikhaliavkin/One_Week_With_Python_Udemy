9b3a6ebc403dbf07a2ac229b48ada5b13b4f7690
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-03 23:46:24 +0200

    Unit 9. Booleans examples

--- Unit 9. Booleans/booleans.py
+++ Unit 9. Booleans/booleans.py
@@ -2,3 +2,43 @@ is_adult = True
 print(is_adult)
 print(type(is_adult))
 
+is_equal = 10 == 5
+print(is_equal)
+print(type(is_equal))
+
+is_not_equal = 10 != 5
+print(is_not_equal)
+print(type(is_not_equal))
+
+is_greater = 10 > 5
+print(is_greater)
+print(type(is_greater))
+
+is_less = 10 < 5
+print(is_less)
+print(type(is_less))
+
+is_greater_or_equal = 10 >= 5
+print(is_greater_or_equal)
+print(type(is_greater_or_equal))
+
+is_less_or_equal = 10 <= 5
+print(is_less_or_equal)
+print(type(is_less_or_equal))
+
+is_int_equal = 10 == 10.0
+print(is_int_equal)
+print(type(is_int_equal))
+
+is_str_equal_to_int = "10" == 10
+print(is_str_equal_to_int)
+print(type(is_str_equal_to_int))
+
+str_compare = "10" > "5"
+print(str_compare)
+print(type(str_compare))
+
+
+
+
+
