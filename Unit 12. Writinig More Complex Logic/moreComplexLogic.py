625bc9af08aec15cebb9882716b1296b1ee0f062
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-04 12:40:18 +0200

    Unit 12. Writinig More COmplex Logic (logical operators example)

--- Unit 12. Writinig More Complex Logic/moreComplexLogic.py
+++ Unit 12. Writinig More Complex Logic/moreComplexLogic.py
@@ -4,3 +4,49 @@ print('True and True is', True and True)
 print('True and False is', True and False)
 print('False and True is', False and True)
 print('False and False is', False and False)
+
+#logical AND operator
+print('True and True is', True & True)
+print('True and False is', True & False)
+print('False and True is', False & True)
+print('False and False is', False & False)
+
+#OR operator
+print('True or True is', True or True)
+print('True or False is', True or False)
+print('False or True is', False or True)
+print('False or False is', False or False)
+
+#logical OR operator
+print('True or True is', True | True)
+print('True or False is', True | False)
+print('False or True is', False | True)
+print('False or False is', False | False)
+
+#logical NOT operator
+print('not True is', not True)
+print('not False is', not False)
+
+#logical XOR operator
+print('True ^ True is', True ^ True)
+print('True ^ False is', True ^ False)
+print('False ^ True is', False ^ True)
+print('False ^ False is', False ^ False)
+
+
+
+#logical implication operator
+print('True >> True is', True >> True)
+print('True >> False is', True >> False)
+print('False >> True is', False >> True)
+print('False >> False is', False >> False)
+
+#logical equivalence operator
+print('True << True is', True << True)
+print('True << False is', True << False)
+print('False << True is', False << True)
+print('False << False is', False << False)
+
+
+
+
