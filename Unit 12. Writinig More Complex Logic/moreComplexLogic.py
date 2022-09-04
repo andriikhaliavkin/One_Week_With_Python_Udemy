451b45732272090d94cb1b2a5fa998500c3b9e18
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-04 10:54:17 +0200

    Unit 12. Writinig More COmplex Logic init

---
+++ Unit 12. Writinig More Complex Logic/moreComplexLogic.py
@@ -0,0 +1,6 @@
+#AND and OR operators
+#AND operator
+print('True and True is', True and True)
+print('True and False is', True and False)
+print('False and True is', False and True)
+print('False and False is', False and False)
