6d781bf2e5aefe5faba23153164c22b1c8b15ce1
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-05 14:30:14 +0200

    Unit 18. Lists Basics

---
+++ Unit 18. Lists The Basics/listsBasics.py
@@ -0,0 +1,35 @@
+#create list
+myList = [1, 2, 3, 4, 5]
+print(myList)
+#add to list
+myList.append(6)
+print(myList)
+#remove from list
+myList.remove(6)
+print(myList)
+#insert into list
+myList.insert(0, 0)
+print(myList)
+#sort list
+myList.sort()
+print(myList)
+#reverse list
+myList.reverse()
+print(myList)
+#count list
+print(myList.count(1))
+#length of list
+print(len(myList))
+#access list
+print(myList[0])
+
+#create task list
+taskList = []
+#add tasks
+taskList.append("Wash the car")
+taskList.append("Clean the house")
+taskList.append("Do the dishes")
+taskList.append("Mow the lawn")
+taskList.append("Take out the trash")
+#display tasks
+print(taskList)
