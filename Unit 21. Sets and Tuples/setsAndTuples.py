dbf7b164799e0fcd952a8377ac03ab22f7d201fc
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-05 18:59:17 +0200

    Unit 21 sets and tuples examples

---
+++ Unit 21. Sets and Tuples/setsAndTuples.py
@@ -0,0 +1,18 @@
+#create set
+set1 = set()
+set1.add(1)
+set1.add(2)
+set1.add(3)
+set1.add(4)
+set1.add(5)
+set1.add(6)
+set1.add(7)
+set1.add(8)
+set1.add(9)
+set1.add(10)
+print(set1)
+#add to set
+
+#create tuple
+tuple1 = (1,2,3,4,5,6,7,8,9,10)
+print(tuple1)
