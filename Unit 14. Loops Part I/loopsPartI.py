b0c515afbf72a10b952b54e0346655a679a18aec
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-05 09:36:06 +0200

    Unit 14. for and while loops examples

---
+++ Unit 14. Loops Part I/loopsPartI.py
@@ -0,0 +1,33 @@
+#for loops
+for i in range(10):
+    print(i)
+print("------------------")
+for i in range(1, 11):
+    print(i)
+print("------------------")
+for i in range(1, 11, 2):
+    print(i)
+print("------------------")
+for i in range(10, 0, -1):
+    print(i)
+print("------------------")
+for i in range(10, 0, -2):
+    print(i)
+print("------------------")
+for i in range(10, 0, -2):
+    print(i, end=" ")
+print("------------------")
+for i in range(10, 0, -2):
+    print(i, end=" ")
+print("------------------")
+#while loops
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+print("------------------")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+print("------------------")
