cc9271205dfe13b860367331f75996a5936bd9d2
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-05 23:12:20 +0200

    Unit 24 modules example

---
+++ Unit 24. Modules/modules.py
@@ -0,0 +1,26 @@
+#import modules exemple
+import math
+print(math.sqrt(25))
+print(math.pi)
+print(math.e)
+print(math.sin(0))
+print(math.cos(0))
+print(math.tan(0))
+print(math.sin(math.pi/2))
+print(math.cos(math.pi/2))
+print(math.tan(math.pi/2))
+print(math.sin(math.pi))
+
+#import daytime module
+import datetime
+print(datetime.datetime.now())
+print(datetime.datetime.now().year)
+print(datetime.datetime.now().month)
+print(datetime.datetime.now().day)
+
+#import random module
+import random
+print(random.random())
+print(random.randint(1, 10))
+
+
